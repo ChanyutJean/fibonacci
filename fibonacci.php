78c142f09dcf3593dff35fbb14ff5f3e2b3af833
ChanyutJean <chanyut22@hotmail.com>
2021-09-27 15:38:03 +0700

    php

--- fibonacci.php
+++ fibonacci.php
@@ -1,3 +1,15 @@
 <?php 
-    echo "42\n"
+    if ($argv[1] < 1 || $argv[1] > 92) {
+        throw new Exception("Index must be a positive integer between 1 and 92.");
+    } 
+    $previous = 0;
+    $current = 1;
+    $next = 0;
+    for ($iteration = 1; $iteration < $argv[1]; $iteration++) {
+        $next = $previous + $current;
+        $previous = $current;
+        $current = $next;
+    }
+    echo $current;
+    echo "\n";
 ?>
