bd67cd1d6c7ff4ab83320d930684e7dbfd7b0bf8
ChanyutJean <chanyut22@hotmail.com>
2021-09-27 16:50:47 +0700

    initial state

--- fibonacci.php
+++ fibonacci.php
@@ -2,10 +2,10 @@
     if ($argv[1] < 1 || $argv[1] > 92) {
         throw new Exception("Index must be a positive integer between 1 and 92.");
     } 
-    $previous = 0;
+    $previous = 1;
     $current = 1;
     $next = 0;
-    for ($iteration = 1; $iteration < $argv[1]; $iteration++) {
+    for ($iteration = 2; $iteration < $argv[1]; $iteration++) {
         $next = $previous + $current;
         $previous = $current;
         $current = $next;
